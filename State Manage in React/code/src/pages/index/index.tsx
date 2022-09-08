fd672a8962fa6ecd20b7be1f6dc12329a9becad1
yangxiaoyao <376476191@qq.com>
2022-09-08 16:00:06 +0800

    Dev (#38)
    * feat:update 100questions chapter4/5
    
    * feat:update 100questions chapter6
    
    * feat:update 100questions chapter6
    
    * feat:update 100questions chapter7
    
    * feat:update 100questions chapter8
    
    * feat:update 100questions chapter9
    
    * feat:update 100questions chapter10
    
    * feat:update 100questions chapter11
    
    * feat:update 100questions chapter2
    
    * feat:update 100questions chapter2/3
    
    * feat:update 100questions chapter2/3
    
    * feat:update framework & interview chapter7
    
    * feat:update framework & interview chapter8
    
    * feat:update articles  Effective TypeScript  http2 in action  react-docs  draft.js
    
    * feat:update framework & interview chapter9
    
    * feat:update framework & interview chapter10:webpack/babel
    
    * fix:remove .DS_Store
    
    * Delete .DS_Store
    
    * feat:update framework & interview chapter11:项目设计
    
    * feat:update framework & interview chapter12:项目流程
    
    * feat:update framework & interview chapter14:React Hooks
    
    * fix:content format
    
    * feat:update react-docs
    
    * feat:update fundamental interview chapter2
    
    * feat:update fundamental interview chapter3:CSS
    
    * feat:update fundamental interview chapter4:JS基础 - 变量类型
    
    * feat:update fundamental interview chapter5:JS基础 - 原型和原型链
    
    * feat:update fundamental interview chapter6:JS基础 - 作用域和闭包
    
    * feat:update fundamental interview chapter7:JS基础 - 异步
    
    * feat:update fundamental interview chapter8:JS异步进阶
    
    * feat:update fundamental interview chapter9-13:JS Web API
    
    * feat:update draft.js
    
    * feat:update fundamental interview chapter14:http面试题
    
    * feat:update fundamental interview chapter15:开发环境
    
    * feat:update fundamental interview chapter16:运行环境
    
    * feat:update fundamental interview chapter17:课程总结
    
    * feat:update fundamental interview chapter18:真题模拟
    
    * fix:dailyQuestions format & type fix
    
    * feat:update simulated interviews:candidate 1
    
    * feat:update simulated interviews:candidate 2
    
    * feat:update simulated interviews:candidate 2
    
    * feat:update simulated interviews:candidate 3
    
    * feat:init interview questions
    
    * feat:update interview series
    
    * feat:update JsGreen Chapter5
    
    * feat:reorganize design pattern series and init a new part:7 Essential Design Patterns
    
    * feat:update 7 Essential Design Patterns chapter3:设计原则
    
    * feat:update 7 Essential Design Patterns chapter4:工厂模式
    
    * feat:update 7 Essential Design Patterns chapter5:单例模式
    
    * feat:update 7 Essential Design Patterns chapter6:观察者模式
    
    * feat:update 7 Essential Design Patterns chapter7:迭代器模式
    
    * feat:update 7 Essential Design Patterns chapter8:原型模式
    
    * feat:update 7 Essential Design Patterns chapter9:装饰器模式
    
    * feat:update typescript@4.8.2
    
    * feat:update interview questions
    
    * feat:init State Manage in React project
    
    * feat:update State Manage in React chapter3:完成搭建工程架构

--- State Manage in React/code/src/pages/index/index.tsx
+++ State Manage in React/code/src/pages/index/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Button } from 'antd';
+import { Button, DatePicker } from 'antd';
+import moment from 'moment';
+import axios from 'axios';
 
-interface Props {}
+import './style.scss';
 
-class IndexPage extends React.Component<Props> {
+class IndexPage extends React.Component {
+  componentDidMount(): void {
+    axios
+      .get('/ad/index/gray')
+      .then((res) => {
+        console.log('res', res.data);
+      })
+      .catch((error) => {
+        console.log('error', error);
+      });
+  }
+
+  handleDateChange(this: void, date: moment.Moment | null) {
+    console.log('date change', moment(date).unix(), this);
+  }
+
   render() {
     return (
-      <div className="box">
-        <span>This is a index page</span>
-        <Button type="primary">按钮</Button>
+      <div className="index-page">
+        <div className="middle-box">
+          <span>This is a index page</span>
+
+          <Button type="primary">按钮</Button>
+        </div>
+        <div>
+          <DatePicker onChange={this.handleDateChange} />
+        </div>
       </div>
     );
   }
--- webpack.common.js
+++ webpack.common.js
@@ -6,10 +6,13 @@ const srcPath = path.join(__dirname, 'src');
 const config = {
   entry: path.join(srcPath, 'index.tsx'),
   resolve: {
-    // extensions: ['.js','.jsx','.ts','.tsx'],
+    extensions: ['.js','.jsx','.ts','.tsx'],
     alias: {
       // pages:path.join(__dirname,'./src/pages')
       pages: path.join(__dirname, './State Manage in React/code/src/pages'),
+      // pages: "/Users/yangxiaoyao/Desktop/code/draft/State Manage in React/code/src/pages",
+      "@utils": path.join(__dirname, './State Manage in React/code/src/utils/'),
+      // "@utils": "/Users/yangxiaoyao/Desktop/code/draft/State Manage in React/code/src/utils",
     },
   },
   plugins: [
